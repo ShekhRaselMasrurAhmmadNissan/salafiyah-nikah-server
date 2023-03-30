dd4e75151dc4a6a5f30f409936ba0ad8e3264239
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2023-03-30 19:18:53 +0600

    Initialized Mongodb.

--- index.js
+++ index.js
@@ -8,9 +8,26 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+	try {
+		const db = client.db('SalafiyahNikah');
+		const UsersCollection = db.collection('Users');
+	} finally {
+	}
+};
+
 app.get('/', async (req, res) => {
 	res.send('The server is running successfully.');
 });
+run().catch((err) => console.log(err));
 
 app.listen(port, () => {
 	console.log(`The server is running at port ${port}`);
